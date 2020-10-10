08bbd567d0eced28e02996f2817d4652ad2d3b06
Racheal Colbert <rachealcolbert16@gmail.com>
2020-10-10 01:18:11 -0700

    prompts for password

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,28 @@ function writePassword() {
 }
 
 function generatePassword() {
+
+  var passwordLength = window.prompt("How many character would you like in your password? ", "8");
+  console.log('passwordLength', passwordLength, typeof passwordLength);
+
+  // confirm: lowercase, uppercase, numeric, and or special characters 
+  var lowercase = window.prompt("Do you want your password to contain lowercase letters?");
+  console.log('lowercase', lowercase, typeof lowercase);
+
+  var uppercase = window.prompt("How about uppercase letters?");
+  console.log('uppercase', uppercase, typeof uppercase);
+
+  var numeric = window.prompt("Do you want numbers in your password?");
+  console.log('numeric', numeric, typeof numeric);
+
+  var specialCharacters = window.prompt("And would you like your password to contain special characters?");
+  console.log('specialCharacter', specialCharacters, typeof specialCharacters);
+
+
+  // generate a password 
+
+
+
   return 5;
 }
 
