581ae20a49e3c886b51e6351c33e34cdf26984ed
Racheal Colbert <rachealcolbert16@gmail.com>
2020-10-10 20:31:48 -0700

    generator

--- Develop/script.js
+++ Develop/script.js
@@ -16,42 +16,45 @@ function writePassword() {
 function generatePassword() {
 
   var passwordLength = prompt("How many character would you like in your password? ", "8");
-  passwordLength = Number.parseInt(passwordLength, 8);
-
+  passwordLength = Number.parseInt(passwordLength, 10);
+  console.log('passwordLength', passwordLength, typeof passwordLength);
   //if size is NaN, then return an error 
   if (Number.isNaN(passwordLength)) {
     alert("Invalid format! Please enter a valid number between 8 and 128.");
+    return '';
   }
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Invalid length! Please use a number between 8 and 128.");
     return '';
   }
-  console.log('passwordLength', passwordLength, typeof passwordLength);
-
-  // confirm: lowercase, uppercase, numeric, and or special characters 
-  var lowercase = prompt("Do you want your password to contain lowercase letters?");
-  if (lowercase === "yes" || lowercase === "Yes") {
-    lowercase = true;
-  } else if (lowercase === "no" || lowercase === "No") {
-    lowercase = false;
-  } else {
-    alert("Invalid input! Please type yes or no.");
-    return '';
-  }
+  var lowercase = confirm("Do you want your password to contain lowercase letters? Press OK to add lowercase letters to your password. Press CANCEL if you don't want lowercase letters in your password.");
   console.log('lowercase', lowercase, typeof lowercase);
 
-  var uppercase = window.prompt("How about uppercase letters?");
+  var uppercase = confirm("How about uppercase letters? Press OK to add uppercase letters to your password. Press CANCEL if you don't want uppercase letters in your password.");
   console.log('uppercase', uppercase, typeof uppercase);
 
-  var numeric = window.prompt("Do you want numbers in your password?");
+  var numeric = confirm("Do you want numbers in your password? Press OK to add numbers and CANCEL to exclude numbers.");
   console.log('numeric', numeric, typeof numeric);
 
-  var specialCharacters = window.prompt("And would you like your password to contain special characters?");
+  var specialCharacters = confirm("And would you like your password to contain special characters? Press OK to add special characters and CANCEL to exclude them.");
   console.log('specialCharacter', specialCharacters, typeof specialCharacters);
 
 
-  // generate a password 
+  var randomPassword = [];
 
+  if (lowercase = true) {
+    randomPassword = randomPassword + lowercaseDigits.concat(lowercase);
+  }
+
+  var allDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var nonZeroDigits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var lowercaseDigits = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseDigits = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var characters = ['@', '#', '$', '%', '<', '!', '<', '?'];
+
+
+
+  return 0;
 }
 
 // Add event listener to generate button
