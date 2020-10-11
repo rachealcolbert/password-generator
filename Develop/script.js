1e769f4d2c27cea17be4a1724a4080156179e971
Racheal Colbert <rachealcolbert16@gmail.com>
2020-10-10 20:45:28 -0700

    generator edits

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+var allDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var nonZeroDigits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercaseDigits = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseDigits = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var characters = ['@', '#', '$', '%', '<', '!', '<', '?'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -45,16 +49,17 @@ function generatePassword() {
   if (lowercase = true) {
     randomPassword = randomPassword + lowercaseDigits.concat(lowercase);
   }
+  if (uppercase = true) {
+    randomPassword = randomPassword + uppercaseDigits.concat(uppercase);
+  }
+  if (numeric = true) {
+    randomPassword = randomPassword + nonZeroDigits.concat(numeric);
+  }
+  if (specialCharacters = true) {
+    randomPassword = randomPassword + characters.concat(specialCharacters);
+  }
 
-  var allDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var nonZeroDigits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var lowercaseDigits = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var uppercaseDigits = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var characters = ['@', '#', '$', '%', '<', '!', '<', '?'];
-
-
-
-  return 0;
+  return randomPassword
 }
 
 // Add event listener to generate button
